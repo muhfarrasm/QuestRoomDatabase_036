96da916563f042bcf994cbdf478660089de12456
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-16 22:20:44 +0700

    menambahkan fun getAllMhs() dan fun getMhs(nim: String)

--- app/src/main/java/com/example/mvvmdatabase/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/mvvmdatabase/repository/RepositoryMhs.kt
@@ -1,7 +1,16 @@
 package com.example.mvvmdatabase.repository
 
 import com.example.mvvmdatabase.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
+
+    // method ini untuk memanggil fungsi geetAllMahasiswa dari mahasiswaDao untuk
+    //mendapatkan semua data dlm bentuk aliran
+    fun getAllMhs() : Flow<List<Mahasiswa>>
+    //method ini untuk memanggil fungsi geetAllMahasiswa dari mahasiswaDao berdasarkan NIM
+    fun getMhs(nim: String) : Flow<List<Mahasiswa>>
+
+    //Method ini memanfaatkan fungsi insertMahasiswa dari MahasiswaDao
     suspend fun insertMhs(mahasiswa: Mahasiswa)
 }
