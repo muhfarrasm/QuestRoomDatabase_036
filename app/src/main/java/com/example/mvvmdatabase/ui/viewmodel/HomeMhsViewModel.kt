7837b805c7f9a05ea0f1f3e5b37e21732a645e09
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-17 01:26:57 +0700

    menambahkan .map, .onStart, .catch

--- app/src/main/java/com/example/mvvmdatabase/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/mvvmdatabase/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,17 +1,50 @@
 package com.example.mvvmdatabase.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mvvmdatabase.data.entity.Mahasiswa
 import com.example.mvvmdatabase.repository.RepositoryMhs
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel(){
-
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessages = it.message ?:"Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
 
 }
 
