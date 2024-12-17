151e1463fc1992614a24bf0960890bd310b0e01f
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-17 10:12:03 +0700

    menambahkan data class DetailUiState, fun Mahasiswa.toDetailUiEvent()

---
+++ app/src/main/java/com/example/mvvmdatabase/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.mvvmdatabase.ui.viewmodel
+
+import com.example.mvvmdatabase.data.entity.Mahasiswa
+
+class DetailMhsViewModel {
+}
+
+
+
+
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+// Data class untuk menampung data yang akan ditampilkan di Ui
+
+//memindahkan data dari Entity ke ui
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent{
+    return MahasiswaEvent(
+        nim =nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+
+    )
+}
