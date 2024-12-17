d2c133437aeba9bc263adbe6152d084c52d51b5d
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-17 15:41:15 +0700

    mendeklarasikan variable

--- app/src/main/java/com/example/mvvmdatabase/ui/view/mahasiswa/UpdateMhsView.kt
+++ app/src/main/java/com/example/mvvmdatabase/ui/view/mahasiswa/UpdateMhsView.kt
@@ -1,6 +1,9 @@
 package com.example.mvvmdatabase.ui.view.mahasiswa
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mvvmdatabase.ui.viewmodel.PenyediaViewModel
@@ -13,6 +16,12 @@ fun UpdateMhsView(
     modifier: Modifier = Modifier,
     viewModel: UpdateMhsViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val uiState = viewModel.updateUIState // Ambil UI State Dari ViewModel
+    val snackBarHostState = remember { SnackbarHostState() } // Ambil Snackbar Host State Dari ViewModel
+    val coroutineScope = rememberCoroutineScope() // Ambil Coroutine Scope Dari ViewModel
+
+    // Observasi Perubahan Snackbar Message Dari ViewModel
+
 
 
 }
