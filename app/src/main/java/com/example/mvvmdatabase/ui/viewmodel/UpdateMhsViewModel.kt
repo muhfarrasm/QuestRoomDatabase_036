2030c2212867b7f129f59913d03143982daf8015
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-17 14:03:37 +0700

    menambahkan class UpdateMhsViewModel

---
+++ app/src/main/java/com/example/mvvmdatabase/ui/viewmodel/UpdateMhsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.mvvmdatabase.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.mvvmdatabase.repository.RepositoryMhs
+
+class UpdateMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+
+}
