d79477c38fe8143a3eaac3611db5a8b7110fef84
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-17 15:43:29 +0700

    membuat effect launch (LaunchedEffect)

--- app/src/main/java/com/example/mvvmdatabase/ui/view/mahasiswa/UpdateMhsView.kt
+++ app/src/main/java/com/example/mvvmdatabase/ui/view/mahasiswa/UpdateMhsView.kt
@@ -1,13 +1,16 @@
 package com.example.mvvmdatabase.ui.view.mahasiswa
 
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mvvmdatabase.ui.viewmodel.PenyediaViewModel
 import com.example.mvvmdatabase.ui.viewmodel.UpdateMhsViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun UpdateMhsView(
@@ -21,7 +24,20 @@ fun UpdateMhsView(
     val coroutineScope = rememberCoroutineScope() // Ambil Coroutine Scope Dari ViewModel
 
     // Observasi Perubahan Snackbar Message Dari ViewModel
-
+    LaunchedEffect(uiState.snackBarMessage) {
+        println("LaunchedEffect Triggered")
+        uiState.snackBarMessage?.let { message ->
+            println("Snackbar Message Received: $message")
+            coroutineScope.launch {
+                println("Launching Coroutine For Snackbar")
+                snackBarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Long
+                )
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
 
 
 }
