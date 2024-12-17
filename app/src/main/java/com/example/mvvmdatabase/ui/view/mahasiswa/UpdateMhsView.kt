15818ba1159e41abb98dca34e19815f9460c0f98
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-17 15:46:00 +0700

    membuat   Scaffold

--- app/src/main/java/com/example/mvvmdatabase/ui/view/mahasiswa/UpdateMhsView.kt
+++ app/src/main/java/com/example/mvvmdatabase/ui/view/mahasiswa/UpdateMhsView.kt
@@ -1,16 +1,26 @@
 package com.example.mvvmdatabase.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mvvmdatabase.ui.customwidget.TopAppBar
 import com.example.mvvmdatabase.ui.viewmodel.PenyediaViewModel
 import com.example.mvvmdatabase.ui.viewmodel.UpdateMhsViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun UpdateMhsView(
@@ -39,5 +49,40 @@ fun UpdateMhsView(
         }
     }
 
-
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackBarHostState)},
+        topBar = {
+            TopAppBar(
+                judul = "Edit Mahasiswa",
+                showBackButton = true,
+                onBack = onBack
+            )
+        }
+    ){
+        padding ->
+        Column (
+            modifier = Modifier.padding(padding)
+                .fillMaxSize()
+                .padding(16.dp)
+        ){
+            InsertBodyMhs(
+                uiState = uiState,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent) // Update State Pada ViewModel
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()) {
+                            viewModel.updateData()
+                            delay(600)
+                            withContext(Dispatchers.Main) {
+                                onNavigate() // Navigasi Di Main Thread
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
 }
