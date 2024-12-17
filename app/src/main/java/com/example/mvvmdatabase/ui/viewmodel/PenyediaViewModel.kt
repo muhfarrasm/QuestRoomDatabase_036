5283a55baaac714aa885594fc4c364850a5d7cfb
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-17 14:19:20 +0700

    menambah initializer DetailMhsViewModel  PenyediaViewModel.kt

--- app/src/main/java/com/example/mvvmdatabase/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/mvvmdatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -4,6 +4,7 @@ import android.text.Editable.Factory
 import android.view.View
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -20,6 +21,12 @@ object PenyediaViewModel{
 
         initializer {
             HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
                 krsApp().containerApp.repositoryMhs
             )
         }
