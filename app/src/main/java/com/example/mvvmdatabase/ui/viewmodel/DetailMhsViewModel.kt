97f1874e6f41614bf2f8b1f83dff6245b6d0a775
muhfarrasm <muhammadfarrasm@gmail.com>
2024-12-17 10:21:33 +0700

    menambahkan class DetailMhsViewModel

--- app/src/main/java/com/example/mvvmdatabase/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/mvvmdatabase/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,8 +1,54 @@
 package com.example.mvvmdatabase.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mvvmdatabase.data.entity.Mahasiswa
+import com.example.mvvmdatabase.repository.RepositoryMhs
+import com.example.mvvmdatabase.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
-class DetailMhsViewModel {
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel(){
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?:"Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            )
+        )
+
 }
 
 
